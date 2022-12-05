7d7e4c35cfea770be45ce99a7239dfa664c572dc
Пользователь <konkoskodor@gmail.com>
2022-12-05 13:19:26 +0200

    Laboratory work

---
+++ UtilLibrary/LabTwo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using McMaster.Extensions.CommandLineUtils;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace UtilLibrary
+{
+    public class LabTwo
+    {
+        public string InputFile = "Files/lab2/input.txt";
+
+        public string OutputFile = "Files/lab2/output.txt";
+
+
+        private static int ConvertToInt32(string value)
+        {
+            bool parsed = Int32.TryParse(value, out int result);
+
+            if (!parsed)
+                throw new ArgumentException($"The value {value} was not parsed to int");
+
+            return result;
+        }
+
+        public void Execute()
+        {
+            string strN = File.ReadAllText(InputFile);
+            int N = ConvertToInt32(strN);
+
+            if (!(1 <= N && N <= 1000))
+            {
+                throw new ArgumentException($"N must be >= 1 and <= 1000");
+            }
+
+            int res = (N + 1) * N / 2 + 1;
+
+            File.WriteAllText(OutputFile, res.ToString());
+        }
+    }
+}
---
+++ practiseWork/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UtilLibrary;
+
+String comand = "";
+//prompt user to enter a command
+Console.WriteLine("Enter a command");
+//read the command
+comand = Console.ReadLine();
+
+if (comand == "version")
+{
+    Console.WriteLine("Version 1.0");
+    Console.WriteLine("By Ilya");
+}
+else if (comand == "run")
+{
+    String lab = "";
+    Console.WriteLine("Enter a lab number");
+    lab = Console.ReadLine();
+    if (lab == "1")
+    {
+        LabOne labOne = new LabOne();
+        labOne.execute();
+    }
+    else if (lab == "2")
+    {
+        LabTwo labTwo = new LabTwo();
+        labTwo.Execute();
+    }
+    else if(lab == "3")
+    {
+        LabThree labThree = new LabThree();
+        labThree.execute();
+    }
+    
+    else
+    {
+        Console.WriteLine("No such lab");
+    }
+   }
+
+else if (comand == "help")
+{
+    Console.WriteLine("run - run the program");
+    Console.WriteLine("version - shows the version of the program");
+    Console.WriteLine("help - shows the list of commands");
+    Console.WriteLine("exit - exits the program");
+}
+else if (comand == "exit")
+{
+    Console.WriteLine("Press any key to exit");
+    Console.ReadKey();
+}
+else
+{
+    Console.WriteLine("Unknown command");
+}
+
+    
